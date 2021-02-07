07828210136a1c5cf09683022d2b8a6c185e9674
Yoelvis Rafael Perez Brito <yoelvis.perez.20017@gmail.com>
2021-02-07 00:31:19 -0400

    Creando UsuariosBLL closes #8

---
+++ BLL/UsuariosBLL.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PrimerRegistroCompleto.DAL;
+using PrimerRegistroCompleto.Entidades;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrimerRegistroCompleto.BLL
+{
+    public class UsuariosBLL
+    {
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Usuarios.Any(e => e.UsuarioId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                //buscar la entidad que se desea eliminar
+                var usuarios = contexto.Usuarios.Find(id);
+
+                if (usuarios != null)
+                {
+                    contexto.Usuarios.Remove(usuarios);//remover la entidad
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+
+        public static Usuarios Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Usuarios usuarios;
+
+            try
+            {
+                usuarios = contexto.Usuarios.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return usuarios;
+        }
+
+        private static bool Insertar(Usuarios usuarios)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Usuarios.Add(usuarios);
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Usuarios usuarios)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(usuarios).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Guardar(Usuarios usuarios)
+        {
+            if (!Existe(usuarios.UsuarioId))
+                return Insertar(usuarios);
+            else
+                return Modificar(usuarios);
+        }
+
+        private static List<Usuarios> GetList(Expression<Func<Usuarios, bool>> criterio)
+        {
+            List<Usuarios> lista = new List<Usuarios>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                //Obtener la lista y filtrarla según el criterio recibido por parámetro
+                lista = contexto.Usuarios.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
