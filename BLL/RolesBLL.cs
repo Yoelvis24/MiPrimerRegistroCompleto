b076eaedf7c0c924c1cdbbeedfd82bc68bd7cae8
Yoelvis Rafael Perez Brito <yoelvis.perez.20017@gmail.com>
2021-02-07 00:33:36 -0400

    creando RolesBLL closes #9

---
+++ BLL/RolesBLL.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PrimerRegistroCompleto.DAL;
+using PrimerRegistroCompleto.Entidades;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrimerRegistroCompleto.BLL
+{
+    public class RolesBLL
+    {
+        private static bool Insertar(Roles rol)
+        {
+
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Roles.Add(rol);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Roles rol)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(rol).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        private static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Roles.Any(e => e.RolId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+        public static bool ExisteDescripcion(string descripcion)
+        {
+
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Roles.Any(e => e.Descripcion == descripcion);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+        public static bool Guardar(Roles rol)
+        {
+
+            if (!Existe(rol.RolId))
+                return Insertar(rol);
+            else
+                return Modificar(rol);
+
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var rol = contexto.Roles.Find(id);
+
+                if (rol != null)
+                {
+                    contexto.Roles.Remove(rol);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static List<Roles> GetList(Expression<Func<Roles, bool>> criterio)
+        {
+            List<Roles> Lista = new List<Roles>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                Lista = contexto.Roles.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+        public static List<Roles> GetRoles()
+        {
+            List<Roles> lista = new List<Roles>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Roles.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+        public static Roles Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Roles rol;
+
+            try
+            {
+                rol = contexto.Roles.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return rol;
+        }
+    }
+}
